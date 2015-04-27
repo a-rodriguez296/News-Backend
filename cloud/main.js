7b710a4f4c6a744ce1a0c17e6aa8da97aaeae7a6
alejo80805 <alejo80805@hotmail.com>
2015-04-27 15:39:43 -0500

    Ya genera el promedio. Todavia no lo asigna.

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,39 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+	// Use Parse.Cloud.define to define as many cloud functions as you want.
+	// For example:
+
+	Parse.Cloud.beforeSave("New", function(request, response){
+
+		var newEntity = request.object;
+		newEntity.set("state",0);
+		response.success();
+
+	});
 
 
+	Parse.Cloud.afterSave("Score", function(request){
 
-Parse.Cloud.beforeSave("New", function(request, response){
+		
+		//Sacar la propiedad new del request
+		var newEntity = request.object.get("new");
 
-	console.log("Hola");
+		//Buscar todos los scores asociados a este new
+		var query = new Parse.Query("Score");
+		query.equalTo("new", newEntity);
+		query.find().then(function (results){
 
-});
+
+	    	//crear una variable donde se almacenan todos los scores
+	    	var sum = 0;
+
+	      //Recorrer los scores y sacar el promedio 
+	      for (var i = 0; i < results.length; ++i) {
+	        sum += results[i].get("score");
+	      }
+	      var average =  (sum / results.length);
+	      newEntity.set("average",average);
+
+		 },function(error){
+
+		 });
+	}); 
